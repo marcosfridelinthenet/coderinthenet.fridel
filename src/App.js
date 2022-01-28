860e1cb2735d2ce1226d2fa47813d9a504399611
marcosinthenet <marcosf@inthenet.com.ar>
2022-01-28 19:30:32 -0300

    Clase 4 - Landing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <ItemListContainer/>
     </div>
   );
 }
